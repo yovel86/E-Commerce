ddce2a9982a05da30b31e1530024aae858ef9106
Yovel <yovel.trichy@gmail.com>
2025-11-27 12:24:01 +0530

    Add Api Gateway

---
+++ api-gateway/src/test/java/com/projects/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
